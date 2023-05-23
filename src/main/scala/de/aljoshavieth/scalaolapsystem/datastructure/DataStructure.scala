aa82077d368fb24d75128a19a16fdb50748cd3f0
Aljosha <mail@aljoshavieth.de>
2023-05-23 13:51:30 +0200

    added comment about unpacking arguments

--- src/main/scala/de/aljoshavieth/scalaolapsystem/datastructure/DataStructure.scala
+++ src/main/scala/de/aljoshavieth/scalaolapsystem/datastructure/DataStructure.scala
@@ -1,11 +1,10 @@
 package de.aljoshavieth.scalaolapsystem.datastructure
 import DataStructureType._
-trait DataStructure {
-
-}
+trait DataStructure {}
 
 object DataStructure {
 	def apply(dataStructureType: DataStructureType, args: List[String]): DataStructure = {
+		// In theory, scala provides the _ :* syntax to unpack the arguments, however at this point it is not clear how long the arguments are, so it is not possible here
 		dataStructureType match {
 			case CUSTOMER=> Customer(args.head, args(1), args(2), args(3), args(4), args(5), args(6), args(7))
 			case DATE=> Date(args.head, args(1), args(2), args(3), args(4), args(5), args(6), args(7), args(8), args(9), args(10), args(11), args(12), args(13), args(14), args(15), args(16))
